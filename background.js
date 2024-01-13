017d016251b417afcb9b9efadb01ed5483793464
robot-rover <sam.obrien00@gmail.com>
2024-01-13 11:49:51 -0600

    Seperate databases

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 const dbName = "wikipedia_wrapped";
-const storeName = "history";
-const storeVer = 1;
+const beginStoreName = "begin";
+const endStoreName = "end";
+const locStoreName = "loc";
+const dbVer = 1;
 const tabKey = 'db_key';
 
 console.log("Loaded Extension");
 
-const DBOpenRequest = indexedDB.open(dbName, storeVer)
+const DBOpenRequest = indexedDB.open(dbName, dbVer)
 const ENABLE_SESSIONS = (browser.sessions !== undefined);
 let IS_MOBILE;
 browser.runtime.getPlatformInfo().then(platform => {
@@ -45,66 +47,94 @@ DBOpenRequest.onsuccess = (event) => {
 }
 
 DBOpenRequest.onupgradeneeded = (event) => {
-    console.log("Loaded Database (Upgrading...)")
+    console.log(`Loaded Database (Upgrading from ${event.oldVersion} to ${event.newVersion})`)
     db = event.target.result;
 
     db.onerror = error_fmt("Unable to upgrade database");
 
-    const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true});
-    objectStore.createIndex("title", "title", { unique: false });
-    objectStore.createIndex("time", "time", { unique: false });
-    objectStore.createIndex("dur", "dur", { unique: false });
-    // objectStore.createIndex("lat", "lat", { unique: false });
-    // objectStore.createIndex("lon", "lon", { unique: false });
-    objectStore.createIndex("parent", "parent", { unique: false });
+    const dbOptions = { keyPath: "id", autoIncrement: true };
+
+    const beginStore = db.createObjectStore(beginStoreName, dbOptions);
+    beginStore.createIndex("title", "title");
+    beginStore.createIndex("begin", "begin");
+    beginStore.createIndex("parent", "parent");
+
+    const endStore = db.createObjectStore(endStoreName, dbOptions);
+    endStore.createIndex("end", "end");
+
+    const locStore = db.createObjectStore(locStoreName, dbOptions);
+    locStore.createIndex("lat", "lat");
+    locStore.createIndex("lon", "lon");
 }
 
-function add_record(pageName, accessTime, parent) {
-  const transaction = db.transaction([storeName], 'readwrite');
+function set_loc(key, loc) {
+  const transaction = db.transaction([locStoreName], 'readwrite');
 
-  transaction.onerror = error_fmt("Database transaction failed");
-  const objectStore = transaction.objectStore(storeName);
-  return promisifyResult(objectStore.add( { time: accessTime.toISOString(), title: pageName, dur: 0, parent: parent } )).then((id) => {
-    console.log('Opened ', pageName, ' (id ', id,  ') at ', accessTime.toISOString());
+  transaction.onerror = error_fmt("loc database transaction failed");
+  const locStore = transaction.objectStore(locStoreName);
+  return promisifyResult(locStore.add( { id: key, lat: loc.latitude, lon: loc.longitude } )).then((_key) => {
+    console.log(`Located page (id ${key}) at lat: ${loc.latitude}, lon: ${loc.longitude}`);
+  }, error_fmt("set loc request failed"));
+}
+
+function set_begin(pageName, accessTime, parent) {
+  const transaction = db.transaction([beginStoreName], 'readwrite');
+
+  transaction.onerror = error_fmt("begin database transaction failed");
+  const beginStore = transaction.objectStore(beginStoreName);
+  return promisifyResult(beginStore.add( { begin: accessTime.toISOString(), title: pageName, parent: parent } )).then((id) => {
+    console.log(`Opened ${pageName} (id ${id}) at ${accessTime.toISOString()} from parent (id ${parent})`);
     return id;
-  }, error_fmt("Object store add request failed"));
+  }, error_fmt("set begin request failed"));
 }
 
-function set_close(key, closeTime) {
-  const transaction = db.transaction([storeName], 'readwrite');
-  transaction.onerror = error_fmt("Database transaction failed");
+function set_end(key, closeTime) {
+  const transaction = db.transaction([endStoreName], 'readwrite');
+  transaction.onerror = error_fmt("end database transaction failed");
 
-  const objectStore = transaction.objectStore(storeName);
-  return promisifyResult(objectStore.get( key )).then((record) => {
-    if (record === undefined) {
-      console.log('Warn: attempted to close page that does not exist (id: ', key.toString(), ')');
-      return false;
-    }
-    record.dur = (closeTime - new Date(record.time)) / 1000;
-    return promisifyResult(objectStore.put(record)).then((_key) => {
-      console.log('Closed ', record.title, ' (id ', key, ') w/ duration ', record.dur);
-      return true;
-    }, error_fmt('Object store set request failed'));
-  }, error_fmt("Object store get request failed"));
+  const endStore = transaction.objectStore(endStoreName);
+  return promisifyResult(endStore.add( { end: closeTime.toISOString(), id: key } )).then((_key) => {
+    console.log(`Closed page (id ${key}) at ${closeTime.toISOString()}`);
+  }, error_fmt('set end request failed'));
 }
 
-function navigate_away(tabId, closeTime) {
+async function navigate_away(tabId, closeTime) {
   const tabIdStr = tabId.toString();
-  return browser.storage.local.get([tabIdStr]).then((results) => {
-    if (tabIdStr in results) {
-      return set_close(results[tabIdStr][0], closeTime).then((isValid) => {
-        return browser.storage.local.remove([tabIdStr]).then(() => {
-          if (isValid) {
-            return results[tabIdStr][0];
-          } else {
-            return null;
-          }
-        });
-      })
-    } else {
-      return null;
+  const results = await browser.storage.local.get([tabIdStr]);
+  if (tabIdStr in results) {
+    set_end(results[tabIdStr][0], closeTime);
+    await browser.storage.local.remove([tabIdStr]);
+    return results[tabIdStr][0];
+  } else {
+    return null;
+  }
+}
+
+async function get_all_data() {
+  const transaction = db.transaction([beginStoreName, endStoreName, locStoreName], 'readonly');
+  transaction.onerror = error_fmt("get all database transaction failed");
+
+  const beginStore = transaction.objectStore(beginStoreName);
+  const endStore = transaction.objectStore(endStoreName);
+  const locStore = transaction.objectStore(locStoreName);
+  const stores = [beginStore, endStore, locStore];
+
+  const data = {};
+  const all = stores.map(async store => {
+    const storeData = await promisifyResult(store.getAll());
+    for (const record of storeData) {
+      if (!(record.id in data)) {
+        data[record.id] = {};
+      }
+      const masterRecord = data[record.id];
+      for (const [key, value] of Object.entries(record)) {
+        if (key === 'id') { continue; }
+        masterRecord[key] = value;
+      }
     }
-  })
+  } );
+  await Promise.all(all);
+  return data;
 }
 
 browser.storage.local.get().then((results) => {
@@ -133,7 +163,7 @@ if (ENABLE_SESSIONS) {
   });
 }
 
-browser.webNavigation.onCompleted.addListener(evt => {
+browser.webNavigation.onCompleted.addListener(async evt => {
   // Filter out any sub-frame related navigation event
   if (evt.frameId !== 0) {
     return;
@@ -158,31 +188,35 @@ browser.webNavigation.onCompleted.addListener(evt => {
 
   const accessTime = new Date();
 
-  browser.storage.local.get([evt.tabId.toString()]).then((results) => {
-    if (evt.tabId in results && results[evt.tabId][1] === pageName) {
+  const results = await browser.storage.local.get([evt.tabId.toString()]);
+
+  if (evt.tabId in results && results[evt.tabId][1] === pageName) {
+    // This tab already existed and was just reloaded
+    return;
+  }
+
+  let prevKey = await navigate_away(evt.tabId, accessTime);
+  if (isSearch) {
+    prevKey = null;
+  }
+
+  if (pageName === undefined) {
+    if (ENABLE_SESSIONS) {
+      return await browser.sessions.removeTabValue(evt.tabId, tabKey);
+    } else {
       return;
     }
-
-    return navigate_away(evt.tabId, accessTime).then((prev_key) => {
-      if (pageName === undefined) {
-        if (ENABLE_SESSIONS) {
-          return browser.sessions.removeTabValue(evt.tabId, tabKey);
-        } else {
-          return;
-        }
-      }
-
-      return add_record(pageName, accessTime, isSearch ? null : prev_key).then((key) => {
-        const toSet = Object();
-        toSet[evt.tabId.toString()] = [key, pageName];
-        return browser.storage.local.set(toSet).then(() => {
-          if (ENABLE_SESSIONS) {
-            browser.sessions.setTabValue(evt.tabId, tabKey, [key, pageName]);
-          }
-        });
-      })
-    });
-  });
+  } else {
+    let newKey = await set_begin(pageName, accessTime, prevKey);
+    const toSet = Object();
+    toSet[evt.tabId.toString()] = [newKey, pageName];
+    browser.storage.local.set(toSet);
+    if (ENABLE_SESSIONS) {
+      browser.sessions.setTabValue(evt.tabId, tabKey, [newKey, pageName]);
+    }
+    const loc = await get_location();
+    set_loc(newKey, loc);
+  }
 }, {
   url: [{schemes: ["http", "https"]}]
 });
@@ -194,36 +228,29 @@ browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
 let data_url = undefined;
 let page_url = undefined;
 
-browser.action.onClicked.addListener((tab, onClickData) => {
-  console.log('Hello World');
-  const transaction = db.transaction([storeName], 'readonly');
+browser.action.onClicked.addListener(async (tab, onClickData) => {
+  const data = await get_all_data();
 
-  transaction.onerror = error_fmt("Download transaction failed");
-  transaction.oncomplete = () => { console.log("Download transaction complete!")};
+  if (data_url != undefined) {
+    URL.revokeObjectURL(data_url);
+  }
+  data_url = URL.createObjectURL(new Blob([JSON.stringify(data)], { type: 'application/json', endings: 'native' } ));
+  console.log('Data available at ', data_url);
 
-  const objectStore = transaction.objectStore(storeName);
-  promisifyResult(objectStore.getAll()).then((data) => {
-    if (data_url != undefined) {
-      URL.revokeObjectURL(data_url);
+  if (IS_MOBILE) {
+    if (page_url != undefined) {
+      URL.revokeObjectURL(page_url);
     }
-    data_url = URL.createObjectURL(new Blob([JSON.stringify(data)], { type: 'application/json', endings: 'native' } ));
-    console.log('Data available at ', data_url);
-
-    if (IS_MOBILE) {
-      if (page_url != undefined) {
-        URL.revokeObjectURL(page_url);
+    page_url = URL.createObjectURL(new Blob([get_download_page(data_url)]));
+    browser.tabs.create({active: true, url: page_url});
+  } else {
+    browser.downloads.download({ url: data_url, filename: 'wikipedia_wrapped.json' }).then(
+      (_downloadId) => {
+        console.log('Download Started');
+      },
+      () => {
+        console.log('Download Canceled');
       }
-      page_url = URL.createObjectURL(new Blob([get_download_page(data_url)]));
-      browser.tabs.create({active: true, url: page_url});
-    } else {
-      browser.downloads.download({ url: data_url, filename: 'wikipedia_wrapped.json' }).then(
-        (_downloadId) => {
-          console.log('Download Started');
-        },
-        () => {
-          console.log('Download Canceled');
-        }
-      );
-    }
-  });
+    );
+  }
 });
